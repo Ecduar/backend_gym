0a63daac4d833b23039bc9b59eceadbf444119d1
Ecduar Estrada <estradanieto@uvm.edu.ve>
2025-06-11 22:05:47 -0400

    Inicializar estructura base del sistema

---
+++ app.js
@@ -0,0 +1,44 @@
+
+const express = require('express');
+const path = require('path');
+
+// Importar routers
+const indexRouter = require('./routes/index');
+const clientesRouter = require('./routes/clientes');
+const entrenadoresRouter = require('./routes/entrenadores');
+//resto
+const membresiasRouter = require('./routes/membresias');
+const pagosRouter = require('./routes/pagos');
+const espaciosRouter = require('./routes/espacios');
+const metodosPagoRouter = require('./routes/metodos-pago');
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Archivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Motor de vistas
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Usar rutas
+app.use('/', indexRouter);
+app.use('/clientes', clientesRouter);
+app.use('/entrenadores', entrenadoresRouter);
+//resto
+app.use('/membresias', membresiasRouter);
+app.use('/pagos', pagosRouter);
+app.use('/espacios', espaciosRouter);
+app.use('/metodos-pago', metodosPagoRouter);
+
+
+
+// Puerto
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
