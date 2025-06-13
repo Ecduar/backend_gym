38908da21213e8374a2f220ef7933e6e23f600d2
danyer1234567 <josemontilla021204@gmail.com>
2025-06-12 21:33:51 -0400

    Agregar rutas y controlador para métodos de pago

---
+++ routes/metodos-pago.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const MetodoPagoController = require('../controllers/metodosPagoController');
+const ctrl = new MetodoPagoController();
+
+router.get('/', (req, res) => res.json(ctrl.listar()));
+router.post('/', (req, res) => res.json(ctrl.agregar(req.body)));
+router.put('/:id', (req, res) => res.json(ctrl.editar(req.params.id, req.body)));
+router.delete('/:id', (req, res) => res.json(ctrl.eliminar(req.params.id)));
+
+module.exports = router;
